1d7f3e5755af0591f2abd38e76e4727caa7f497b
Muhammad Mehboob <qariabidhussainceo@gmail.com>
2026-09-21 10:48:06 +0500

    Add Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -8,5 +9,12 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body>{children}</body></html>;
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <GoogleAnalytics gaId="G-RF037RB6YX" />
+      </body>
+    </html>
+  );
 }
