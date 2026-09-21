08936f1991bf726a6ddb64073e6464dbfadf3e10
Muhammad Mehboob <qariabidhussainceo@gmail.com>
2026-09-22 00:56:19 +0500

    fix: correct component import paths for slug pages

--- app/(public)/blog/[slug]/page.tsx
+++ app/(public)/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
-import { Nav, Footer } from "../../../components/site";
+import { Nav, Footer } from "../../../../components/site";
 
 const SURL = "https://flrccmjaiyutynhydxeo.supabase.co";
 const SKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZscmNjbWphaXl1dHluaHlkeGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MDI4MDQsImV4cCI6MjEwNTQ3ODgwNH0.u0Qd1doTLrghPh2iVQ6PfoM2vnLX7rDQJqcHh2t03I0";
